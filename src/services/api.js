597606637ed34d6b81b387003613cdd2c1ddaf8f
Roberto Torres <robertorres.live@gmail.com>
2022-07-08 23:19:39 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://api.hgbrasil.com/weather?format=json-cors&key=e167cd53",
+    baseURL: "https://api.hgbrasil.com/weather?format=json-cors&key=c9816580",
 });
 
 export default api;
